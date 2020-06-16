6a434e5f0056fb8263a5f665c10d8a537164972d
Anna Silva <annadasilva89@hotmail.com>
2020-06-16 15:21:34 -0300

    c#

---
+++ logica2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace logica2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nome = "Adalberto";
+            byte idade = 40;
+            char genero = 'M';
+            Console.Write(nome + "tem" + idade + "anos" + "do sexo" + genero);
+            Console.ReadLine();
+        }
+    }
+}
